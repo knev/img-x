240404e22609ca068054d0caeb1af492969c6620
knev <knev@users.noreply.github.com>
2026-06-12 21:08:30 +0200

    chore: tweak output.

--- x-exif.sh
+++ x-exif.sh
@@ -63,11 +63,11 @@ for F in "${farray[@]}"; do
 
 	OK=`echo $FN | grep -o '[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}_[0-9]\{2\}.[0-9]\{2\}.[0-9]\{2\}'`
 	if [ -z "$OK" ]; then
-		echo "NO DateTime extracted"
+		echo "  NO DateTime extracted"
 		continue;
 	fi
 
-	echo $FN
+	echo "  [$FN]"
 	$cmd "${F%.*}.${F##*.}" "$FN.${F##*.}"
 	if [ -f "${F%.*}.XMP" ]; then
 		$cmd "${F%.*}.XMP" "$FN.XMP"
